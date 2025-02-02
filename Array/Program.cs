acb48f6b1cbfddd8045fc9edfda20fcd58a31d34
BaraaAlArab <baraa9000alarab@gmail.com>
2025-02-02 23:23:24 +0200

    removed the Comments

--- Array/Program.cs
+++ Array/Program.cs
@@ -11,39 +11,39 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            //CoffeeShope coffeeShope = new CoffeeShope();
-            //coffeeShope.AddCoffees();
-            //coffeeShope.DisplayCoffees();
+            CoffeeShope coffeeShope = new CoffeeShope();
+            coffeeShope.AddCoffees();
+            coffeeShope.DisplayCoffees();
 
-            //ItemCollection collection = new ItemCollection();
-            //collection.AddItems();
-            //collection.DisplayItems();
+            ItemCollection collection = new ItemCollection();
+            collection.AddItems();
+            collection.DisplayItems();
 
-            //Console.WriteLine("this array is not dynamic data\n");
-            //var arlist = new ArrayList() { 100, "Baraa", "KIA", 5.2 };
-            //var arlist2 = new ArrayList() { 200, "Toyota", 52 };
-            //arlist.AddRange(arlist2);
-            //arlist2.AddRange(arlist);
-            //Console.Write("ArrayList Element are:  ");
-            //for (int i = 0; i < arlist.Count; i++)
-            //{
-            //    Console.Write(arlist[i] + "   ");
-            //}
+            Console.WriteLine("this array is not dynamic data\n");
+            var arlist = new ArrayList() { 100, "Baraa", "KIA", 5.2 };
+            var arlist2 = new ArrayList() { 200, "Toyota", 52 };
+            arlist.AddRange(arlist2);
+            arlist2.AddRange(arlist);
+            Console.Write("ArrayList Element are:  ");
+            for (int i = 0; i < arlist.Count; i++)
+            {
+                Console.Write(arlist[i] + "   ");
+            }
             var arr = new ArrayList() { 1, "HP", 300, 4.5 };
             var arr1 = new ArrayList() { 0, "Toyota", 4000, 5.5 };
-            //int firstElement = (int)arr[0];
-            //string secondElement = (string)arr[1];
+            int firstElement = (int)arr[0];
+            string secondElement = (string)arr[1];
             arr1.InsertRange(2, arr);
-            //arr.Insert(1, "BMW");
+            arr.Insert(1, "BMW");
             foreach (var item2 in arr1)
             {
                 Console.WriteLine(item2);
             }
-            //foreach (var item in arr)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //Console.WriteLine(firstElement + " " + secondElement);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(firstElement + " " + secondElement);
 
         }
     }
